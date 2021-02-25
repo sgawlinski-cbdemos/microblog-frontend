45c52ba6126310639b390c91afaa832784e246dc
sgawlinskicb <78441591+sgawlinskicb@users.noreply.github.com>
2021-02-25 14:06:01 -0600

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -3,7 +3,7 @@ import store from '../store'
 import { betaAccess } from './users'
 
 export const Flags = {
-  sidebar: new Rox.Flag(false),
+  sidebar: new Rox.Flag(false)
   title: new Rox.Flag(false)
 }
 
